bb4383e9eb093ddf56030ae6039f8028c89deb99
Migwe101 <105769844+Migden@users.noreply.github.com>
2024-05-21 10:12:10 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y gdb wget curl
+
+# Install OhMyZSH
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/master/tools/install.sh)"
+
+# Change .bashrc to show MOTD
+
+echo "echo '  __  __ _                   __  ___  __ '" >> ~/.bashrc
+echo "echo ' |  \/  (_)                 /_ |/ _ \/_ |'" >> ~/.bashrc
+echo "echo ' | \  / |_  __ ___      _____| | | | || |'" >> ~/.bashrc
+echo "echo ' | |\/| | |/ _  \ \ /\ / / _ \ | | | || |'" >> ~/.bashrc
+echo "echo ' | |  | | | (_| |\ V  V /  __/ | |_| || |'" >> ~/.bashrc
+echo "echo ' |_|  |_|_|\__, | \_/\_/ \___|_|\___/ |_|'" >> ~/.bashrc
+echo "echo '            __/ |                        '" >> ~/.bashrc
+echo "echo '           |___/                         '" >> ~/.bashrc
+echo "echo '-----------------------------------------'" >> ~/.bashrc
+echo "echo ' Date: \$(date)                          '" >> ~/.bashrc
